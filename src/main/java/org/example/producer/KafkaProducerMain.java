01cf04b81b701ab9e5bfadbc0196f4a325f0eec6
devfcph <devfcph@gmail.com>
2023-09-15 12:47:16 -0600

    Refactor producer

--- src/main/java/org/example/producer/KafkaProducerMain.java
+++ src/main/java/org/example/producer/KafkaProducerMain.java
@@ -1,15 +1,22 @@
 package org.example.producer;
 
 import org.apache.kafka.clients.producer.*;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Properties;
+import java.util.Random;
+import java.util.UUID;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 
-public class KafkaProducerExample {
+public class KafkaProducerMain {
     public static void main(String[] args) {
-        String bootstrapServers = "localhost:9092"; // Cambia esto por la dirección de tu servidor Kafka
+        String bootstrapServers = "localhost:9092";
         String topicName = "TP-PAGOS-CAJA-CATALOGOS-SUCURSALES-V1-AUTOSERVICIOS-ADD";
 
         // Configuración del productor
@@ -18,17 +25,15 @@ public class KafkaProducerExample {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.ACKS_CONFIG, "all"); // Garantía de entrega de al menos una vez
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 10); // Tamaño del lote (10 en este caso)
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 10); // Tamaño del lote
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
         try {
             for (int i = 0; i < 100; i++) {
-                // Construye tu mensaje de acuerdo al esquema
                 String message = buildMessage(i);
                 ProducerRecord<String, String> record = new ProducerRecord<>(topicName, message);
 
-                // Envía el mensaje al topic
                 producer.send(record, new Callback() {
                     @Override
                     public void onCompletion(RecordMetadata metadata, Exception exception) {
@@ -51,29 +56,29 @@ public class KafkaProducerExample {
             ObjectMapper objectMapper = new ObjectMapper();
             ObjectNode messageNode = objectMapper.createObjectNode();
 
-            // Construye el objeto 'transaccionautoservicios'
+
             ObjectNode transaccionNode = objectMapper.createObjectNode();
-            transaccionNode.put("id", "4924f0a2-6b22-4690-b01a-0ff215b7ac10");
-            transaccionNode.put("fechaHora", "2023-01-02T15:18:26.400-06:00");
+            transaccionNode.put("id", String.valueOf(UUID.randomUUID()));
+            transaccionNode.put("fechaHora", getTimestamp());
             transaccionNode.put("nombre", "PAGOS-CAJA-CATALOGOS-SUCURSALES-V1-AUTOSERVICIOS");
             transaccionNode.put("accion", "ADD");
             transaccionNode.put("fechaInicioProceso", "2023-01-02T15:18:26.400-06:00");
             transaccionNode.put("fechaFinProceso", "2023-01-02T15:18:26.400-06:00");
 
-            // Construye el objeto 'operacion'
+
             ObjectNode operacionNode = objectMapper.createObjectNode();
             operacionNode.put("idPais", 1);
             operacionNode.put("idCanal", 1);
-            operacionNode.put("numero", 4624);
+            operacionNode.put("numero", i + 1);
             operacionNode.put("numeroCajero", 2);
             operacionNode.put("numeroMultifuncional", 3);
             operacionNode.put("numeroCajeroAutoservicio", 3);
             operacionNode.put("anioSemanaPago", 202318);
-            operacionNode.put("multifuncional", true);
+            operacionNode.put("multifuncional", new Random().nextBoolean());
             operacionNode.put("metaAutoservicio", 500);
-            operacionNode.put("fechaHoraRegistroCentral", "2023-05-08T15:18:26.400-06:00");
+            operacionNode.put("fechaHoraRegistroCentral", getTimestamp());
 
-            // Combina 'transaccion' y 'operacion' en el mensaje
+
             messageNode.set("transaccion", transaccionNode);
             messageNode.set("operacion", operacionNode);
 
@@ -82,5 +87,9 @@ public class KafkaProducerExample {
             e.printStackTrace();
             return "";
         }
+    }
+
+    private static String getTimestamp() {
+        return String.valueOf(new Timestamp(new Date().getTime()));
     }
 }
